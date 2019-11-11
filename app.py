59db609007f4f77095c785a49f893cf49a22c310
Fred <fredzhang2013@gmail.com>
2019-11-10 23:10:07 -0800

    enable trafficlight alternate - red1 on, green2 on

--- app.py
+++ app.py
@@ -6,47 +6,93 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-ledRed = 13
-ledYellow= 19
-ledGreen= 26
-ledRedSts = 0
-ledYellowSts = 0
-ledGreenSts = 0
-GPIO.setup(ledRed, GPIO.OUT)
-GPIO.setup(ledYellow,GPIO.OUT)
-GPIO.setup(ledGreen, GPIO.OUT)
-GPIO.output(ledRed, GPIO.LOW)
-GPIO.output(ledYellow, GPIO.LOW)
-GPIO.output(ledGreen, GPIO.LOW)
+
+ledRed1 = 21
+ledYellow1 = 20
+ledGreen1 = 16
+
+ledRedSts1 = 0
+ledYellowSts1 = 0
+ledGreenSts1 = 0
+
+ledRed2 = 26
+ledYellow2 = 19
+ledGreen2 = 13
+
+ledRedSts2 = 0
+ledYellowSts2 = 0
+ledGreenSts2 = 0
+
+GPIO.setup(ledRed1, GPIO.OUT)
+GPIO.setup(ledYellow1,GPIO.OUT)
+GPIO.setup(ledGreen1, GPIO.OUT)
+GPIO.output(ledRed1, GPIO.LOW)
+GPIO.output(ledYellow1, GPIO.LOW)
+GPIO.output(ledGreen1, GPIO.LOW)
+
+GPIO.setup(ledRed2, GPIO.OUT)
+GPIO.setup(ledYellow2,GPIO.OUT)
+GPIO.setup(ledGreen2, GPIO.OUT)
+GPIO.output(ledRed2, GPIO.LOW)
+GPIO.output(ledYellow2, GPIO.LOW)
+GPIO.output(ledGreen2, GPIO.LOW)
+
 @app.route('/')
 def index():
-    ledRedSts = GPIO.input(ledRed)
-    ledYellowSts = GPIO.input(ledYellow)
-    ledGreenSts = GPIO.input(ledGreen)
-    templateData = { 'ledRed' : ledRedSts,
-    'ledYellow' : ledYellowSts,
-    'ledGreen' : ledGreenSts }
+    ledRedSts1 = GPIO.input(ledRed1)
+    ledYellowSts1 = GPIO.input(ledYellow1)
+    ledGreenSts1 = GPIO.input(ledGreen1)
+    ledRedSts2 = GPIO.input(ledRed2)
+    ledYellowSts2 = GPIO.input(ledYellow2)
+    ledGreenSts2 = GPIO.input(ledGreen2)
+    templateData = {
+    'ledRed1' : ledRedSts1,
+    'ledYellow1' : ledYellowSts1,
+    'ledGreen1' : ledGreenSts1,
+    'ledRed2' : ledRedSts2,
+    'ledYellow2' : ledYellowSts2,
+    'ledGreen2' : ledGreenSts2 }
     return render_template('index.html', **templateData)
 @app.route('/<deviceName>/<action>')
 def do(deviceName, action):
-    if deviceName == "ledRed":
-        actuator = ledRed
-    if deviceName == "ledYellow":
-        actuator = ledYellow
-    if deviceName == "ledGreen":
-        actuator = ledGreen
+    if deviceName == "ledRed1":
+        actuator = ledRed1
+    if deviceName == "ledYellow1":
+        actuator = ledYellow1
+    if deviceName == "ledGreen1":
+        actuator = ledGreen1
+    if deviceName == "ledRed2":
+        actuator = ledRed2
+    if deviceName == "ledYellow2":
+        actuator = ledYellow2
+    if deviceName == "ledGreen2":
+        actuator = ledGreen2
     if action == "on":
         GPIO.output(actuator, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(actuator, GPIO.LOW)
+        if actuator == ledRed1:
+            GPIO.output(ledGreen2, GPIO.HIGH)
+        if actuator == ledGreen1:
+            GPIO.output(ledRed2, GPIO.HIGH)
     if action == "off":
         GPIO.output(actuator, GPIO.LOW)
-    ledRedSts = GPIO.input(ledRed)
-    ledYellowSts = GPIO.input(ledYellow)
-    ledGreenSts = GPIO.input(ledGreen)
-    templateData = { 'ledRed' : ledRedSts,
-    'ledYellow' : ledYellowSts,
-    'ledGreen' : ledGreenSts }
+    ledRedSts1 = GPIO.input(ledRed1)
+    ledYellowSts1 = GPIO.input(ledYellow1)
+    ledGreenSts1 = GPIO.input(ledGreen1)
+    
+    ledRedSts2 = GPIO.input(ledRed2)
+    ledYellowSts2 = GPIO.input(ledYellow2)
+    ledGreenSts2 = GPIO.input(ledGreen2)
+    
+    templateData = {
+    'ledRed1' : ledRedSts1,
+    'ledYellow1' : ledYellowSts1,
+    'ledGreen1' : ledGreenSts1,
+    'ledRed2' : ledRedSts2,
+    'ledYellow2' : ledYellowSts2,
+    'ledGreen2' : ledGreenSts2 }
+    
     return render_template('index.html', **templateData )
+
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug=True)
+
---
+++ gpio_app.py
@@ -0,0 +1,52 @@
+# Raspberry Pi 3 GPIO Pins Status And Control Using Flask Web Server and Python
+
+import RPi.GPIO as GPIO
+import time
+from flask import Flask, render_template, request
+app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+ledRed = 13
+ledYellow= 19
+ledGreen= 26
+ledRedSts = 0
+ledYellowSts = 0
+ledGreenSts = 0
+GPIO.setup(ledRed, GPIO.OUT)
+GPIO.setup(ledYellow,GPIO.OUT)
+GPIO.setup(ledGreen, GPIO.OUT)
+GPIO.output(ledRed, GPIO.LOW)
+GPIO.output(ledYellow, GPIO.LOW)
+GPIO.output(ledGreen, GPIO.LOW)
+@app.route('/')
+def index():
+    ledRedSts = GPIO.input(ledRed)
+    ledYellowSts = GPIO.input(ledYellow)
+    ledGreenSts = GPIO.input(ledGreen)
+    templateData = { 'ledRed' : ledRedSts,
+    'ledYellow' : ledYellowSts,
+    'ledGreen' : ledGreenSts }
+    return render_template('index.html', **templateData)
+@app.route('/<deviceName>/<action>')
+def do(deviceName, action):
+    if deviceName == "ledRed":
+        actuator = ledRed
+    if deviceName == "ledYellow":
+        actuator = ledYellow
+    if deviceName == "ledGreen":
+        actuator = ledGreen
+    if action == "on":
+        GPIO.output(actuator, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(actuator, GPIO.LOW)
+    if action == "off":
+        GPIO.output(actuator, GPIO.LOW)
+    ledRedSts = GPIO.input(ledRed)
+    ledYellowSts = GPIO.input(ledYellow)
+    ledGreenSts = GPIO.input(ledGreen)
+    templateData = { 'ledRed' : ledRedSts,
+    'ledYellow' : ledYellowSts,
+    'ledGreen' : ledGreenSts }
+    return render_template('index.html', **templateData )
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug=True)
